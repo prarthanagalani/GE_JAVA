99f01dea4d28d8346c4796c54f644dee5400a1ab
prarthanagalani <prarthanagalani@gmail.com>
2023-11-21 19:09:55 +0530

    added welcome message

---
+++ EmployeeWageProblem_day2/EmployeeWage.java
@@ -0,0 +1,11 @@
+package GE_JAVA.EmployeeWageProblem_day2;
+
+
+
+public class EmployeeWage{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+
+}
